f20a1d1600f97a7766865920548e7874ab3a0ebd
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2020-12-02 12:41:10 +0800

    Add exit

---
+++ Assets/scripts/exit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public void Quit() {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
