634ac4d3e8b0f311e70d60e97480a0c5317b50ab
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2020-12-02 15:33:38 +0800

    Add main camera move with wasd

---
+++ Assets/scripts/Move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    int Speed = 1;
+
+    void MoveForward() {
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+    }
+    void MoveBack() { 
+        transform.Translate(Vector3.forward * Time.deltaTime * -Speed) ;
+    }
+    void MoveLeft() { 
+        transform.Translate(Vector3.left * Time.deltaTime * Speed);
+    }
+    void MoveRight() {
+        transform.Translate(Vector3.right * Time.deltaTime * -Speed);
+    }
+    // void Lrotate() {
+    //     transform.Rotate(Vector3.up * Time.deltaTime * Speed); 
+    // }
+    // void Rrotate() {
+    //     transform.Rotate(Vector3.up * Time.deltaTime * -Speed);
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) {
+            MoveForward();
+            Debug.Log("W");
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            MoveLeft();
+            Debug.Log("A");
+        }
+        if(Input.GetKey(KeyCode.S)) {
+            MoveBack();
+            Debug.Log("S");
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            MoveRight();
+            Debug.Log("D");
+        }
+        // if (Input.GetKey(KeyCode.Q)) {
+        //     //Lrotate();
+        //     Debug.Log("Q");
+        // }
+        // if (Input.GetKey(KeyCode.E)) {
+        //     //Rrotate();
+        //     Debug.Log("E");
+        // }
+    }
+}
