23b71670e8437c5defcb52623accf302584d3c14
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2020-12-27 13:43:02 +0800

    Update the version and add the wall colliders

---
+++ Assets/scripts/Disappear.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    public void Open() {
+        
+        //if (OnMouseDown() && (true)) {
+            Destroy(this.gameObject);
+        //}
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 My First Unity 3D Game
 
 ## Tools
-- Unity 2020.1.15f1c1
+- Unity 2020.1.17f1c1
 
 ## Aim
 An escape room
