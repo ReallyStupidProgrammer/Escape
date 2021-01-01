dcf0839ec85d4bf69444b077128e4e3a746bb742
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-01 15:30:27 +0800

    Add drawer password

--- Assets/scripts/Drawer.cs
+++ Assets/scripts/Drawer.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class Drawer : MonoBehaviour
 {
     IEnumerator coroutine;
-
+    public static int[] password = {0, 0, 0};
     IEnumerator drag(GameObject canvas) {
         while (true) {
             if (Input.GetMouseButton(0)) {
                 float mouseY = Input.GetAxis("Mouse Y") * 0.1f;
-                if (transform.localPosition.x <= -2.0 && mouseY < 0) ;
-                else if (transform.localPosition.x >= 0 && mouseY > 0) ;
-                else {
+                if ((transform.localPosition.x >= -2.0 || mouseY > 0) 
+                && (transform.localPosition.x <= 0 || mouseY < 0)) {
                     transform.Translate(mouseY, 0, 0, Space.Self);
                     canvas.transform.Translate(0, 0, mouseY, Space.Self);
                 }
@@ -20,7 +19,17 @@ public class Drawer : MonoBehaviour
             yield return null;
         }
     }
-    public void open() {
+    bool check(string correct) {
+        for (int i = 0; i < correct.Length; i ++) {
+            if ((int) correct[i] - 48 != password[i]) 
+                return false;
+        }
+        return true;
+    }
+    public void open(string correct) {
+        if (gameObject.tag == "locked" && check(correct)) {
+            gameObject.tag = "unlocked";    
+        }
         if (gameObject.tag == "unlocked") {
             GameObject canvas = GameObject.Find("closetCanvas");
             coroutine = drag(canvas);
--- Assets/scripts/Password.cs
+++ Assets/scripts/Password.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class Password : MonoBehaviour
 {
-    public void add() {
-        Text pwd = gameObject.GetComponent<Text>();
-        int num = int.Parse(pwd.text);
-        num = (num + 1) % 10;
-        pwd.text = num.ToString();
+    public void add(string item_name) {
+        string item_tag = GameObject.Find(item_name).tag;
+        if (item_tag == "locked") {
+            Text pwd = gameObject.GetComponent<Text>();
+            int num = int.Parse(pwd.text);
+            num = (num + 1) % 10;
+            pwd.text = num.ToString();
+            Drawer.password[int.Parse(gameObject.name)] = num;
+        }
     }
 }
