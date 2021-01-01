b8892732916bb07b90bb6bca0ab24650b404bf65
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-01 13:24:01 +0800

    Complete open drawer

--- Assets/scripts/Drawer.cs
+++ Assets/scripts/Drawer.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Drawer : MonoBehaviour
 {
+    IEnumerator coroutine;
+
+    IEnumerator drag(GameObject canvas) {
+        while (true) {
+            if (Input.GetMouseButton(0)) {
+                float mouseY = Input.GetAxis("Mouse Y") * 0.1f;
+                if (transform.localPosition.x <= -2.0 && mouseY < 0) ;
+                else if (transform.localPosition.x >= 0 && mouseY > 0) ;
+                else {
+                    transform.Translate(mouseY, 0, 0, Space.Self);
+                    canvas.transform.Translate(0, 0, mouseY, Space.Self);
+                }
+            }
+            yield return null;
+        }
+    }
     public void open() {
-        if (Input.GetMouseButton(1)) {
-            float mouseY = Input.GetAxis("mouseY") * 3;
-            transform.Translate(mouseY, 0, 0, Space.Self);
+        if (gameObject.tag == "unlocked") {
+            GameObject canvas = GameObject.Find("closetCanvas");
+            coroutine = drag(canvas);
+            StartCoroutine(coroutine);
         }
     }
 }
