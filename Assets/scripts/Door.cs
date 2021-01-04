bf673d5d500ea52c34ab9059ef05914dde1a1cd1
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-04 18:02:16 +0800

    Complete all door open operations

--- Assets/scripts/Door.cs
+++ Assets/scripts/Door.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 
     public float origin;
+    public bool reverse;
+
+    public bool leftRight;
+
+    float transfer(float temp) {
+        return (temp + 360) % 360;
+    }
 
     public void open() {
         float mouseX = Input.GetAxis("Mouse X") * 3;
-        float current = transform.eulerAngles.y;
-        if (current + mouseX > origin + 90) return;
-        if (current + mouseX < origin) return;
+        float current = transform.localEulerAngles.y;
+        if (reverse) {
+            mouseX *= -1;
+        }
+            //print(mouseX);
+            // print(current);
+
+         //else {
+        if (leftRight) {
+            if (transfer(current + mouseX) > transfer(origin + 90)) return;
+            if (transfer(current + mouseX) < origin) return;
+        } else {
+            if (transfer(current + mouseX) < transfer(origin - 90)) return;
+            if (transfer(current + mouseX) > origin) return;
+        }
+        // }
+
         transform.Rotate(0, mouseX, 0, Space.World);
     }
 }
