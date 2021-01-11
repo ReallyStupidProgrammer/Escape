3fe87e8633ad248ae4e4d387f68e1273e1d0ed85
reallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-11 19:00:29 +0800

    Add wall, floor and roof textures

--- Assets/scripts/start.cs
+++ Assets/scripts/start.cs
@@ -7,4 +7,8 @@ public class Start : MonoBehaviour {
     public void start() {
         SceneManager.LoadSceneAsync(1);
     }
+
+    public void Menu() {
+        SceneManager.LoadSceneAsync(0);
+    }
 }
