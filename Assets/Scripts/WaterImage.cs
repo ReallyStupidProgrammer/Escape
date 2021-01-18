bf2977463b1ea867597a30da70deb3e46cd75fee
ReallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-18 22:30:58 +0800

    Add pipes

---
+++ Assets/Scripts/WaterImage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterImage : MonoBehaviour {
+    
+    public GameObject water;
+    public float position;
+
+    private void Update() {
+        if (water.transform.position.y >= position) {
+            gameObject.layer = 0;
+        } else {
+            gameObject.layer = 8;
+        }
+    }
+}
