8c348909a04e40f8546f0d8b3d6b213c678a4f38
ReallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-19 21:54:04 +0800

    Add purpleBall operations

---
+++ Assets/Scripts/PurpleBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBall : MonoBehaviour {
+
+    public GameObject basin;
+    private bool dropped = false;
+
+    private void Update() {
+        if (dropped) return;
+        if (basin.layer == 0) {
+            dropped = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -31,7 +31,6 @@ public class Rotation : MonoBehaviour {
         if (reverse) {
             mouseX *= -1;
         }
-        print(transform.eulerAngles);
         if (leftRight) {
             if (transfer(current + mouseX, false) > transfer(origin + 90, true)) return;
             if (transfer(current + mouseX, false) < transfer(origin, false)) return;
@@ -39,7 +38,6 @@ public class Rotation : MonoBehaviour {
             if (transfer(current + mouseX, false) < transfer(origin - 90, false)) return;
             if (transfer(current + mouseX, false) > transfer(origin, true)) return;
         }
-        print("test");
         if (xyz == 0)
             transform.Rotate(mouseX, 0, 0, Space.World);
         else if (xyz == 1)
