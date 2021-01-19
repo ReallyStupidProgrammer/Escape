7b093df638e24bfcc7b8e52fc6e86d949441053a
ReallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-01-19 17:34:01 +0800

    Add CD operation

---
+++ Assets/Scripts/CD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CD : MonoBehaviour {
+
+    public GameObject relatedObject;
+    public string objectName;
+    public Sprite picture;
+
+    private bool added = false;
+
+    private void Update() {
+        if (!added && relatedObject.GetComponent<Item>().collected) {
+            added = true;
+            gameObject.AddComponent<Item>();
+            gameObject.GetComponent<Item>().objectName = objectName;
+            gameObject.GetComponent<Item>().picture = picture;
+            gameObject.tag = "item";
+        }
+    }
+}
