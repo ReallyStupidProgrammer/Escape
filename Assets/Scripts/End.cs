761f91b5c555e7a383c464c9f9f96e0790c12add
ReallyStupidProgrammer <xumouyi_at_wflms@163.com>
2021-05-05 23:18:35 +0800

    Add the end position

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour {
+
+    public GameObject relatedDoor = null;
+    public GameObject endLight = null;
+
+    private void Update() {
+        if (relatedDoor.tag == "unlocked") {
+            endLight.SetActive(true);
+        }
+    }
+}
